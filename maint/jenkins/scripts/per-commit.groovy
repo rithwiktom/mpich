a4522029efdc913b56aab14890a757576e823f01
Wesley Bland <wesley.bland@intel.com>
2021-07-22 10:40:13 -0500

    jenkins: Use enable-ch4-mt all the time

--- maint/jenkins/scripts/per-commit.groovy
+++ maint/jenkins/scripts/per-commit.groovy
@@ -456,7 +456,6 @@ fi
 # Force configurations if merging with main due to some incompatibilities
 if [ "\${GITHUB_PR_TARGET_BRANCH}" == "main" -o "\${GITHUB_PR_TARGET_BRANCH}" == "integration_main" ]; then
     gpudirect="no"
-    mt_model="main"
     json="no"
 fi
 
