982a1f8e1c8319f3b64024cb8a82a8c6dc0acab6
Alex Brooks <alex.brooks@intel.com>
2021-09-15 23:58:17 -0500

    jenkins: implement enable/disable support for xfail parsing
    To comment out or uncomment a particular line in an xfail file, use
    'disable' or 'enable', respectively, instead of 'xfail=...'

--- maint/jenkins/scripts/set_xfail.py
+++ maint/jenkins/scripts/set_xfail.py
@@ -4,6 +4,7 @@ import os
 
 class G:
     opts = {'jobname': '-', 'compiler': '-', 'config': '-', 'provider': '-', 'direct': '-', 'am': '-', 'pmix': '-'}
+    disable_enable = {}
     states = []
     xfails = {}
 
@@ -20,6 +21,7 @@ def main():
     parse_args()
     load_xfail_conf()
     apply_xfails()
+    apply_disable_enable()
 
 # ---- subroutines --------------------------------------------
 def parse_args():
@@ -73,6 +75,19 @@ def load_xfail_conf():
                 else:
                     # print(" Unmatched state [%s] - [%s] - %s" % (cond, pat, testlist))
                     pass
+            elif RE.match(r'\s*(.*?)\s*\/(.*)\/\s*((?:dis|en)able)\s*(\S+)\s*$', line):
+                # -- new direct pattern
+                cond, pat, reason, testlist = RE.m.group(1, 2, 3, 4)
+                if match_states(cond, G.states):
+                    cond = re.sub(r'\s\s+', ' ', cond)
+                    if testlist not in G.disable_enable:
+                        G.disable_enable[testlist] = {}
+                        G.disable_enable[testlist]['enable'] = []
+                        G.disable_enable[testlist]['disable'] = []
+                    G.disable_enable[testlist][reason].append({'cond': cond, 'pat': pat, 'reason': reason})
+                else:
+                    # print(" Unmatched state [%s] - [%s] - %s" % (cond, pat, testlist))
+                    pass
             elif RE.match(r'\s*(.*?)\s*\/(.*)\/\s*xfail=([-\/#\w]*)\s*(\S+)\s*$', line):
                 # -- new direct pattern
                 cond, pat, reason, testlist = RE.m.group(1, 2, 3, 4)
@@ -116,6 +131,42 @@ def apply_xfails():
             print("    %s not changed. Is there a mistake? Rules list:" % f, file=sys.stderr)
             for r in rules:
                 print("        %s \t:%s" % (r['pat'], r['reason']), file=sys.stderr)
+
+def apply_disable_enable():
+    for f in sorted (G.disable_enable.keys()):
+        if not os.path.exists(f):
+            print("! testlist: %s does not exist\n" % f)
+            continue
+        for k in sorted(G.disable_enable[f].keys()):
+            rules = G.disable_enable[f][k]
+            lines = []
+            n_applied = 0
+            print("testlist: %s ..." % f)
+            with open(f, "r") as In:
+                for line in In:
+                    for r in rules:
+                        if not RE.search(r'xfail=', line):
+                            if RE.search(r['pat'], line):
+                                print("    %s:\t%s\t-> xfail=%s" % (r['cond'], r['pat'], r['reason']))
+                                if "dryrun" not in G.opts:
+                                    if r['reason'] == "enable":
+                                        if line[0] == "#":
+                                            line = line[1:].rstrip() + "\n"
+                                    else:
+                                        if line[0] != "#":
+                                            line = "#" + line.rstrip() + "\n"
+                                n_applied+=1
+                                break
+                    lines.append(line)
+
+            if n_applied:
+                with open(f, "w") as Out:
+                    for l in lines:
+                        print(l, end='', file=Out)
+            else:
+                print("    %s not changed. Is there a mistake? Rules list:" % f, file=sys.stderr)
+                for r in rules:
+                    print("        %s \t:%s" % (r['pat'], r['reason']), file=sys.stderr)
 
 def match_states(cond, states):
     tlist = cond.split()
