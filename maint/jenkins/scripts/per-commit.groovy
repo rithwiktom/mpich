18cb0f4027dc5f634b64cace23a78b5a32aa3317
Alex Brooks <alex.brooks@intel.com>
2022-06-22 17:44:59 -0500

    jenkins: update ze-native for ats to device id format

--- maint/jenkins/scripts/per-commit.groovy
+++ maint/jenkins/scripts/per-commit.groovy
@@ -503,9 +503,9 @@ if [ "$gpu" = "ats" ]; then
     fi
     xpmem="no"
     # TODO: Switch back to system-installed neo once memid impl is fixed
-    neo_dir=/home/gengbinz/drivers.gpu.compute.runtime/workspace-09-10-2021
+    neo_dir=/usr
     ze_dir=/usr
-    ze_native="$gpu"
+    ze_native="12.1.0"
     disable_psm2="yes"
     CONFIG_EXTRA="\$CONFIG_EXTRA --enable-psm3"
 elif [ "$gpu" = "nogpu" ]; then
