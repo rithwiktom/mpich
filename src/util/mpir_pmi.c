ff09ff712fb6ccd0e54361d5c8d95737ea26af96
Xi Luo <xi.luo@intel.com>
2023-08-30 09:51:34 -0700

    coll: skip the first line in coords file

--- src/util/mpir_pmi.c
+++ src/util/mpir_pmi.c
@@ -1598,10 +1598,14 @@ static int parse_coord_file(const char *filename)
     MPIR_ERR_CHKANDJUMP1(NULL == coords_file, mpi_errno, MPI_ERR_FILE,
                          "**filenoexist", "**filenoexist %s", filename);
 
+    /* Skip the first line */
+    fscanf(coords_file, "%*[^\n]\n");
+
     MPIR_Process.coords_dims = 0;
     fscanf(coords_file, "%d:", &rank);
     while (!feof(coords_file)) {
-        if (fscanf(coords_file, "%d", &rank) == 1)
+        int temp = 0;
+        if (fscanf(coords_file, "%d", &temp) == 1)
             ++MPIR_Process.coords_dims;
         else
             break;
@@ -1611,6 +1615,8 @@ static int parse_coord_file(const char *filename)
 
     MPIR_Assert(MPIR_Process.coords_dims == 3);
     rewind(coords_file);
+    /* Skip the first line */
+    fscanf(coords_file, "%*[^\n]\n");
 
     MPIR_Process.coords =
         MPL_malloc(MPIR_Process.coords_dims * sizeof(int) * MPIR_Process.size, MPL_MEM_COLL);
